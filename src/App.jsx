70483b9cfcf829017b01071132ed4377cd428f19
EstherOqua <66262441+EstherOqua@users.noreply.github.com>
2020-12-14 16:39:33 +0000

    Added Login page w/ logo within a container

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { format } from 'path';
+import React from 'react';
+import './App.scss';
+import { Login } from "./components/login/index";
+
+
+function App() {
+  return (
+    <div className="App">
+     <Login />
+
+
+    </div>
+  );
+}
+
+export default App;
